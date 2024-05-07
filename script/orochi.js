8805aa8cb29366c44e8b39cb79b14656cf12e718
Mdkkdjssnsb <163823860+Mdkkdjssnsb@users.noreply.github.com>
2024-05-07 13:44:42 +0530

    Update orochi.js

--- script/orochi.js
+++ script/orochi.js
@@ -1,1 +1,24 @@
+const axios = require('axios');
 
+module.exports.config = {
+    name: "orochi",
+    version: 1.0,
+    credits: "ArYAN",
+    description: "ask an questions to Orochi Ai",
+    hasPrefix: true,
+    usages: "{pn} [prompt]",
+    aliases: ['chi"],
+    cooldown: 0,
+};
+
+module.exports.run = async function ({ api, event, args }) {
+    try {
+        const prompt = args.join(" ");
+        const response = await axios.get(`https://aryan-apis.onrender.com/orochiai?prompt=${encodeURIComponent(prompt)}`);
+        const answer = response.data.fullResponse;
+
+        await api.sendMessage(answer, event.threadID);
+    } catch (error) {
+        console.error("Error:", error.message);
+    }
+};
