c7448303fef9403308f08e8dd5a3c5c8613084b4
Mdkkdjssnsb <163823860+Mdkkdjssnsb@users.noreply.github.com>
2024-05-07 14:44:08 +0530

    Update lyrics.js

--- script/lyrics.js
+++ script/lyrics.js
@@ -1,6 +1,4 @@
 const axios = require("axios");
-const fs = require("fs-extra");
-const path = require("path");
 
 module.exports.config = {
     name: 'lyrics',
@@ -10,34 +8,31 @@ module.exports.config = {
     usage: "${prefix} [songName]",
     credits: 'ArYAN',
     cooldown: 3,
-	  hasPrefix: true,
+    hasPrefix: true,
 };
 
-  module.exports.run = async function ({ api, event, args }) {
+module.exports.run = async function ({ api, event, args }) {
     try {
-      const songName = args.join(" ");
-      if (!songName) {
-        api.sendMessage(`⛔ 𝗜𝗻𝘃𝗮𝗹𝗶𝗱 𝗧𝗶𝘁𝗹𝗲\n\n➤ Please provide a song name!`, event.threadID, event.messageID);
-        return;
-      }
+        const songName = args.join(" ");
+        if (!songName) {
+            api.sendMessage(`⛔ 𝗜𝗻𝘃𝗮𝗹𝗶𝗱 𝗧𝗶𝘁𝗹𝗲\n\n➤ Please provide a song name!`, event.threadID);
+            return;
+        }
 
-      const apiUrl = `https://aryan-apis.onrender.com/api/lyrics?songName=${encodeURIComponent(songName)}&key=loveyou`;
-      const response = await axios.get(apiUrl);
-      const { lyrics, title, artist, image } = response.data;
+        const apiUrl = `https://aryan-apis.onrender.com/api/lyrics?songName=${encodeURIComponent(songName)}&key=loveyou`;
+        const response = await axios.get(apiUrl);
+        const { lyrics, title, artist, image } = response.data;
 
-      if (!lyrics) {
-        api.sendMessage(`⛔ 𝗡𝗼 𝗗𝗮𝘁𝗮\n\n➤ Sorry, lyrics not found. Please provide another song name!`, event.threadID, event.messageID);
-        return;
-      }
+        if (!lyrics) {
+            api.sendMessage(`⛔ 𝗡𝗼 𝗗𝗮𝘁𝗮\n\n➤ Sorry, lyrics not found. Please provide another song name!`, event.threadID);
+            return;
+        }
 
-      let message = `🎶 𝗟𝗬𝗥𝗜𝗖𝗦\n\nℹ️ 𝗧𝗶𝘁𝗹𝗲\n➪ ${title}\n👑 𝗔𝗿𝘁𝗶𝘀𝘁\n➪ ${artist}\n🔎 𝗟𝘆𝗿𝗶𝗰𝘀\n━━━━━━━━━━━━━━━\n${lyrics}`;
-      let attachment = await global.utils.getStreamFromURL(image);
-      api.sendMessage({ body: message, attachment }, event.threadID, (err, info) => {
-        let id = info.messageID;
-      });
+        let message = `🎶 𝗟𝗬𝗥𝗜𝗖𝗦\n\nℹ️ 𝗧𝗶𝘁𝗹𝗲\n➪ ${title}\n👑 𝗔𝗿𝘁𝗶𝘀𝘁\n➪ ${artist}\n🔎 𝗟𝘆𝗿𝗶𝗰𝘀\n━━━━━━━━━━━━━━━\n${lyrics}`;
+        let attachment = await global.utils.getStreamFromURL(image);
+        api.sendMessage({ body: message, attachment }, event.threadID);
     } catch (error) {
-      console.error(error);
-      api.sendMessage(`⛔ 𝗡𝗼 𝗗𝗮𝘁𝗮\n\n➤ Sorry, lyrics not found. Please provide another song name!`, event.threadID, event.messageID);
+        console.error(error);
+        api.sendMessage(`⛔ 𝗡𝗼 𝗗𝗮𝘁𝗮\n\n➤ Sorry, lyrics not found. Please provide another song name!`, event.threadID);
     }
-  },
 };
