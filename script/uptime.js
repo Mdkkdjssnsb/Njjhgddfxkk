50f249f0cc21f57faa6ad3fb709e3c381e8f057b
Mdkkdjssnsb <163823860+Mdkkdjssnsb@users.noreply.github.com>
2024-05-07 14:48:48 +0530

    Update uptime.js

--- script/uptime.js
+++ script/uptime.js
@@ -1,5 +1,4 @@
 const os = require("os");
-const fs = require("fs-extra");
 
 const startTime = new Date(); // Moved outside onStart
 
@@ -11,53 +10,51 @@ module.exports.config = {
     usage: "${prefix}uptime",
     credits: 'ArYAM',
     cooldown: 0,
-	  hasPrefix: true,
+    hasPrefix: true,
 };
   
-  module.exports.run = async function ({ api, event, args, threadsData, usersData }) {
+module.exports.run = async function ({ api, event }) {
     try {
-      const uptimeInSeconds = (new Date() - startTime) / 1000;
+        const uptimeInSeconds = (new Date() - startTime) / 1000;
 
-      const seconds = uptimeInSeconds;
-      const days = Math.floor(seconds / (3600 * 24));
-      const hours = Math.floor((seconds % (3600 * 24)) / 3600);
-      const minutes = Math.floor((seconds % 3600) / 60);
-      const secondsLeft = Math.floor(seconds % 60);
-      const uptimeFormatted = `${days}d ${hours}h ${minutes}m ${secondsLeft}s`;
+        const seconds = uptimeInSeconds;
+        const days = Math.floor(seconds / (3600 * 24));
+        const hours = Math.floor((seconds % (3600 * 24)) / 3600);
+        const minutes = Math.floor((seconds % 3600) / 60);
+        const secondsLeft = Math.floor(seconds % 60);
+        const uptimeFormatted = `${days}d ${hours}h ${minutes}m ${secondsLeft}s`;
 
-      const loadAverage = os.loadavg();
-      const cpuUsage =
-        os
-          .cpus()
-          .map((cpu) => cpu.times.user)
-          .reduce((acc, curr) => acc + curr) / os.cpus().length;
+        const loadAverage = os.loadavg();
+        const cpuUsage =
+            os
+            .cpus()
+            .map((cpu) => cpu.times.user)
+            .reduce((acc, curr) => acc + curr) / os.cpus().length;
 
-      const totalMemoryGB = os.totalmem() / 1024 ** 3;
-      const freeMemoryGB = os.freemem() / 1024 ** 3;
-      const usedMemoryGB = totalMemoryGB - freeMemoryGB;
+        const totalMemoryGB = os.totalmem() / 1024 ** 3;
+        const freeMemoryGB = os.freemem() / 1024 ** 3;
+        const usedMemoryGB = totalMemoryGB - freeMemoryGB;
 
-      const allUsers = await usersData.getAll();
-      const allThreads = await threadsData.getAll();
-      const currentDate = new Date();
-      const options = { year: "numeric", month: "numeric", day: "numeric" };
-      const date = currentDate.toLocaleDateString("en-US", options);
-      const time = currentDate.toLocaleTimeString("en-US", {
-        timeZone: "Asia/Kolkata",
-        hour12: true,
-      });
+        const currentDate = new Date();
+        const options = { year: "numeric", month: "numeric", day: "numeric" };
+        const date = currentDate.toLocaleDateString("en-US", options);
+        const time = currentDate.toLocaleTimeString("en-US", {
+            timeZone: "Asia/Kolkata",
+            hour12: true,
+        });
 
-      const timeStart = Date.now();
-      await api.sendMessage({
-        body: "🔎| checking........",
-      }, event.threadID);
+        const timeStart = Date.now();
+        await api.sendMessage({
+            body: "🔎| checking........",
+        }, event.threadID);
 
-      const ping = Date.now() - timeStart;
+        const ping = Date.now() - timeStart;
 
-      let pingStatus = "⛔| 𝖡𝖺𝖽 𝖲𝗒𝗌𝗍𝖾𝗆";
-      if (ping < 1000) {
-        pingStatus = "✅| 𝖲𝗆𝗈𝗈𝗍𝗁 𝖲𝗒𝗌𝗍𝖾𝗆";
-      }
-      const systemInfo = `♡   ∩_∩
+        let pingStatus = "⛔| 𝖡𝖺𝖽 𝖲𝗒𝗌𝗍𝖾𝗆";
+        if (ping < 1000) {
+            pingStatus = "✅| 𝖲𝗆𝗈𝗈𝗍𝗁 𝖲𝗒𝗌𝗍𝖾𝗆";
+        }
+        const systemInfo = `♡   ∩_∩
  （„• ֊ •„)♡
 ╭─∪∪────────────⟡
 │ 𝗨𝗣𝗧𝗜𝗠𝗘 𝗜𝗡𝗙𝗢
@@ -71,7 +68,7 @@ module.exports.config = {
 │𝙲𝙿𝚄 𝙼𝙾𝙳𝙴𝙻: ${os.cpus()[0].model}
 │𝚂𝚃𝙾𝚁𝙰𝙶𝙴: ${usedMemoryGB.toFixed(2)} GB / ${totalMemoryGB.toFixed(2)} GB
 │𝙲𝙿𝚄 𝚄𝚂𝙰𝙶𝙴: ${cpuUsage.toFixed(1)}%
-│𝚁𝙰𝙼 𝚄𝚂𝙶𝙴: ${process.memoryUsage().heapUsed / 1024 / 1024} MB;
+│𝚁𝙰𝙼 𝚄𝚂𝙶𝙴: ${process.memoryUsage().heapUsed / 1024 / 1024} MB
 ├───────────────⟡
 │ ✅ 𝗢𝗧𝗛𝗘𝗥 𝗜𝗡𝗙𝗢
 │𝙳𝙰𝚃𝙴: ${date}
@@ -81,29 +78,17 @@ module.exports.config = {
 ╰───────────────⟡
 `;
 
-      api.sendMessage(
-        {
-          body: systemInfo,
-        },
-        event.threadID,
-        (err, messageInfo) => {
-          if (err) {
-            console.error("Error sending message with attachment:", err);
-          } else {
-            console.log(
-              "Message with attachment sent successfully:",
-              messageInfo,
-            );
-          }
-        },
-      );
+        api.sendMessage(
+            {
+                body: systemInfo,
+            },
+            event.threadID
+        );
     } catch (error) {
-      console.error("Error retrieving system information:", error);
-      api.sendMessage(
-        "Unable to retrieve system information.",
-        event.threadID,
-        event.messageID,
-      );
+        console.error("Error retrieving system information:", error);
+        api.sendMessage(
+            "Unable to retrieve system information.",
+            event.threadID
+        );
     }
-  },
 };
